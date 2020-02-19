83d743def0495752771137d9cdc1016a1c28f818
ddelmas <dfdelmas@gmail>
2020-02-19 23:10:34 +0100

    parsing level metadatas

--- Zookoban/scripts/singletons/LevelLibrary.gd
+++ Zookoban/scripts/singletons/LevelLibrary.gd
@@ -2,10 +2,13 @@ extends Node
 
 const  LEVELS_DIR_PATH = "res://levels"
 
+const LEVEL_INFO_FLAG = "]"
+const LEVEL_INFO_SEPARATOR = "|"
+
+var levels = []
+
 func _ready():
 	load_level_files(list_all_level_files(LEVELS_DIR_PATH))
-	
-	
 	
 func list_all_level_files(directory_path):
 	print("Retrieving all files from directory \""+ directory_path+"\" :")
@@ -40,7 +43,22 @@ func load_level_file(file_path):
 		push_error("level file \""+file_path+"\" not found")
 		return 
 
-	levels_file.open("res://levels/levels.tres", File.READ)
-	var content = levels_file.get_as_text()
-	print (content)
+	levels_file.open(file_path, File.READ)
+	while not levels_file.eof_reached():
+			var line = levels_file.get_line()
+			if line.find(LEVEL_INFO_FLAG) != -1:
+				var level_infos = line.trim_prefix(LEVEL_INFO_FLAG).split(LEVEL_INFO_SEPARATOR)
+				var level_world = -1
+				var level_number = -1
+				var level_name = ""
+				if level_infos.size() >=2:
+					level_world = level_infos[0]
+					level_number = level_infos[1]
+				if level_infos.size() >=3:
+					level_name = level_infos[2]
+				print ("parsing level "+level_world+"."+level_number+" : \""+level_name+"\".") 
+					
+	
+	#var content = levels_file.get_as_text()
+	#print (content)
 	levels_file.close()
