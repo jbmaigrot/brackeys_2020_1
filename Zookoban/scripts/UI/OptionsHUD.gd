c2c26e3436124c7959e22745c9643bc9d868926a
damien <none>
2020-02-22 21:42:58 +0100

    background for options ui

--- Zookoban/scripts/UI/OptionsHUD.gd
+++ Zookoban/scripts/UI/OptionsHUD.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$HBoxContainer/VBoxContainer/CheckButtonMusic.connect("toggled",SoundManager,"music_toggled")
-	$HBoxContainer/VBoxContainer/CheckButtonSounds.connect("toggled",SoundManager, "sounds_toggled")
+	$HBoxContainer/PanelContainer/VBoxContainer/CheckButtonMusic.connect("toggled",SoundManager,"music_toggled")
+	$HBoxContainer/PanelContainer/VBoxContainer/CheckButtonSounds.connect("toggled",SoundManager, "sounds_toggled")
 
 
