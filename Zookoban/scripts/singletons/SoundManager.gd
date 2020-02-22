c4a4d33574430b41fbe1395aff286f4a92049ccf
damien <none>
2020-02-22 12:09:11 +0100

    sound manager placeholder

---
+++ Zookoban/scripts/singletons/SoundManager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+var music_volume = 1
+var sounds_volume = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
