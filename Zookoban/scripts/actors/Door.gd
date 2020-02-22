9e49ee5bc1b7d66169b30b5fc80261f4dde86df3
Antoine SARRAZIN <tony78.itouch@gmail.com>
2020-02-22 20:46:47 +0100

    Fixed button behaviour

--- Zookoban/scripts/actors/Door.gd
+++ Zookoban/scripts/actors/Door.gd
@@ -7,6 +7,8 @@ var open = false
 var sprite
 var pos
 
+var btn_list = []
+
 onready var spr_door_open = preload("res://sprites/s_door_open.png")
 onready var spr_door_closed = preload("res://sprites/s_door_closed.png")
 
@@ -18,6 +20,20 @@ func _ready():
 
 
 func _process(delta):
+	
+	if btn_list == []:
+		for node in grid.get_children():
+			if node is ActorButton:
+				btn_list.append(node)
+		print(btn_list)
+	
+	for btn in btn_list:
+		if (btn.active):
+			open = true
+			break
+		else:
+			open = false
+	
 	if grid.grid[pos.x][pos.y] == TYPE.OBSTACLE and open:
 		grid.grid[pos.x][pos.y] = TYPE.EMPTY
 		sprite.texture = spr_door_open
