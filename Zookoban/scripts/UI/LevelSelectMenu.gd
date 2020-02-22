669f3a5ca34d0a902ffc5c67bbbb6c9136a0d26a
damien <none>
2020-02-22 09:54:39 +0100

    small enhancement to the level select menu

--- Zookoban/scripts/UI/LevelSelectMenu.gd
+++ Zookoban/scripts/UI/LevelSelectMenu.gd
@@ -7,13 +7,18 @@ export (String, FILE) var first_scene_temp
 func _ready():
 	$VBoxContainer/Button_Back.connect("pressed", self, "back_button_pressed")
 	
+	var first_world = true
 	for world_idx in LevelLibrary.levels:
 		for level_idx in LevelLibrary.levels[world_idx]:
+			if first_world :
+				$VBoxContainer/GridContainer.columns = LevelLibrary.levels[world_idx].size()
+				
 			var level_info = LevelLibrary.levels[world_idx][level_idx]
 			var button = Button.new()
 			button.text = str(world_idx)+"."+str(level_idx)
 			button.connect("pressed",self, "level_button_pressed", [world_idx,level_idx])
 			$VBoxContainer/GridContainer.add_child(button)
+		first_world = false
 
 	
 func back_button_pressed():
